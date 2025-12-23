ce91a020b1c79dde8736869787e6dc6f1d8de9c8
sagar874-btech <119278761+sagar874-btech@users.noreply.github.com>
2025-12-23 19:43:54 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './App.css';
+function App() {
+  return <div>Habit Tracker PWA</div>;
+}
+export default App;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
